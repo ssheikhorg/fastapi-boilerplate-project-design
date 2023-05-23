93240fd9cda1351d841dca4ce347d1fd902e6008
Shapon Sheikh <47027475+ssheikhorg@users.noreply.github.com>
2023-05-23 11:41:21 +0600

    made monog clients with following mypy strongly typed convension

--- src/core/database/mongo_client.py
+++ src/core/database/mongo_client.py
@@ -1,7 +1,14 @@
+from typing import Any
 from urllib.parse import quote_plus
 
 from motor.motor_tornado import MotorClient
 from pymongo import ReturnDocument
+from pymongo.client_session import ClientSession
+from pymongo.collection import Collection
+
+from pymongo.mongo_client import MongoClient
+from pymongo.results import InsertOneResult, InsertManyResult, UpdateResult, DeleteResult
+from pymongo.server_api import ServerApi
 
 from src import settings as c
 
@@ -10,72 +17,101 @@ _db_url = (
 )
 
 
-class MongoDB:
+class AsyncMongoDB:
     """A helper used to easily interact with a mongodb database."""
 
-    def __init__(self, collection_name: str, db_name: str = "test") -> None:
-        self._collection = MotorClient(c.yl_db_url)[db_name][collection_name]
+    def __init__(self, collection_name: str, db_name: str) -> None:
+        if db_name == c.yl_db_name:
+            self._collection = MotorClient(c.yl_db_url, server_api=ServerApi("1"))[db_name][collection_name]
+        else:
+            self._collection = MotorClient(_ym_db_url, server_api=ServerApi("1"))[db_name][collection_name]
 
-    async def insert_one_document(self, **data: Any) -> Any:
+    async def insert(self, **document: dict) -> InsertOneResult:
         """Insert one document"""
-        return await self._collection.insert_one(data)
+        result: InsertOneResult = await self._collection.insert_one(document)
+        return result
 
-    async def insert_many_document(self, **data: Any) -> Any:
+    async def insert_many(self, **data: dict) -> InsertManyResult:
         """Insert many documents"""
-        return await self._collection.insert_many(data)
+        result: InsertManyResult = await self._collection.insert_many(data)
+        return result
 
-    async def find_one_document(self, query: dict) -> Any:
+    async def get(self, query: dict) -> dict:
         """Find one document"""
-        return await self._collection.find_one(query, {"_id": 0})
+        result: dict = await self._collection.find_one(query)
+        if result:
+            result["_id"] = result["_id"].__str__()
+        return result
 
-    async def find_many_document(self, query: dict, skip: int = 0, limit: int = 10, sort: tuple = None) -> Any:
+    async def get_many(self, query: dict, skip: int = 0, limit: int = 10, sort: tuple = None) -> list[dict]:
         """Find many documents"""
         if sort:
-            return (
-                await self._collection.find(query, {"_id": 0})
+            result: list = (
+                await self._collection.find(query)
                 .sort(sort[0], sort[1])
-                .skip(skip)
+                .skip(skip * limit)
                 .limit(limit)
                 .to_list(length=limit)
             )
-        return await self._collection.find(query, {"_id": 0}).skip(skip).limit(limit).to_list(length=limit)
+        else:
+            result = await self._collection.find(query, {"_id": 0}).skip(skip).limit(limit).to_list(length=limit)
+        return [{k: v.__str__() if k == "_id" else v for k, v in i.items()} for i in result]
 
-    async def count_document(self, query: dict) -> Any:
+    async def count(self, query: dict) -> int:
         """Count documents"""
-        return await self._collection.count_documents(query)
+        result: int = await self._collection.count_documents(query)
+        return result
 
-    async def update_one_document(
-        self, query: dict, data: dict, array_filters: Any = None, upsert: bool = False
-    ) -> Any:
+    async def update(self, query: dict, data: dict, array_filters: list = None, upsert: bool = False) -> UpdateResult:
         """Update one document"""
         if array_filters:
-            return await self._collection.update_one(query, data, array_filters=array_filters, upsert=upsert)
-        return await self._collection.update_one(query, data)
+            result: UpdateResult = await self._collection.update_one(
+                query, data, array_filters=array_filters, upsert=upsert
+            )
+        else:
+            result = await self._collection.update_one(query, data)
+        return result
 
-    async def update_many_document(self, query: dict, data: dict, upsert: bool = False) -> Any:
+    async def update_many(self, query: dict, data: dict, upsert: bool = False) -> Any:
         """Update many documents"""
         return await self._collection.update_many(query, data, upsert=upsert)
 
-    async def find_one_and_update_document(self, query: dict, data: dict) -> Any:
+    async def delete(self, query: dict) -> DeleteResult:
+        """Delete one document"""
+        result: DeleteResult = await self._collection.delete_one(query)
+        return result
+
+    async def aggregate(self, pipeline: list) -> list[dict]:
+        """Aggregate documents"""
+        result = await self._collection.aggregate(pipeline).to_list(length=None)
+        return [{k: v.__str__() if k == "_id" else v for k, v in i.items()} for i in result]
+
+    async def find_and_update(self, query: dict, data: dict) -> dict:
         """Find one and update document"""
         _update_kwargs = {"projection": {"_id": False}, "upsert": True, "return_document": ReturnDocument.AFTER}
-        return await self._collection.find_one_and_update(query, data, **_update_kwargs)
+        result: dict = await self._collection.find_one_and_update(query, data, **_update_kwargs)
+        if result:
+            result["_id"] = result["_id"].__str__()
+        return result
 
-    async def start_session(self) -> Any:
+    async def start_session(self) -> ClientSession:
         """Start session for transaction"""
-        return await self._collection.database.client.start_session()
+        result: ClientSession = await self._collection.database.client.start_session()
+        return result
 
-    async def aggregate_document(self, pipeline: list) -> Any:
-        """Aggregate documents"""
-        return await self._collection.aggregate(pipeline).to_list(length=None)
+    async def check_collection(self) -> bool:
+        """Check if collection exists"""
+        return self._collection.name in await self._collection.database.list_collection_names()
 
-    async def delete_one_document(self, query: dict) -> Any:
-        """Delete one document"""
-        return await self._collection.delete_one(query)
+    async def create_collection(self) -> Collection:
+        """Create collection"""
+        result: Collection = await self._collection.database.create_collection(self._collection.name)
+        return result
 
-    async def create_index(self, index: list) -> Any:
+    async def create_index(self, index: list) -> str:
         """Create index"""
-        return await self._collection.create_index(index)
+        result: str = await self._collection.create_index(index)
+        return result
 
     async def index_exists(self, index: list) -> bool:
         """Check if index exists"""
@@ -86,23 +122,54 @@ class MongoDB:
         return False
 
 
+class SyncMongoDB:
+    """A helper used to easily interact with a mongodb database."""
+
+    def __init__(self, collection_name: str, db_name: str) -> None:
+        if db_name == c.yl_db_name:
+            self._collection = MongoClient(c.yl_db_url, server_api=ServerApi("1"))[db_name][
+                collection_name
+            ]  # type: ignore
+        else:
+            self._collection = MongoClient(_ym_db_url, server_api=ServerApi("1"))[db_name][collection_name]
+
+    def insert(self, data: dict) -> InsertOneResult:
+        result: InsertOneResult = self._collection.insert_one(data)
+        return result
+
+    def get(self, query: dict) -> dict | None:
+        result: dict | None = self._collection.find_one(query)
+        if result:
+            result["_id"] = result["_id"].__str__()
+        return result
+
+
 class MongoEvent:
     """A helper used to easily interact with a mongodb database."""
 
     def __init__(self) -> None:
-        self.client_one = MotorClient("mongodb://localhost:27017/")
-        self.client_two = MotorClient("mongodb://localhost:27017/")
+        self.async_ym_client: MongoClient = MotorClient(_ym_db_url)
+        self.async_yl_client: MongoClient = MotorClient(c.yl_db_url)
+        self.sync_ym_client: MongoClient = MongoClient(_ym_db_url)
+        self.sync_yl_client: MongoClient = MongoClient(c.yl_db_url)
 
     async def start(self) -> tuple:
         """on_event startup mongodb"""
-        return self.ym_client, self.yl_client
+        return self.async_ym_client, self.async_yl_client, self.sync_ym_client, self.sync_yl_client
 
     async def stop(self) -> None:
         """on_event shutdown mongodb"""
-        self.ym_client.close()
-        self.yl_client.close()
+        self.async_ym_client.close()
+        self.async_yl_client.close()
+        self.sync_ym_client.close()
+        self.sync_yl_client.close()
 
 
-def db_collection(collection_name: str, db_name: str) -> MongoDB:
+def db_collection(collection_name: str, db_name: str = c.ym_db_name) -> AsyncMongoDB:
     """Get mongodb database"""
-    return MongoDB(collection_name, db_name)
+    return AsyncMongoDB(collection_name, db_name)
+
+
+def db_sync_collection(collection_name: str, db_name: str = c.ym_db_name) -> SyncMongoDB:
+    """Get mongodb database"""
+    return SyncMongoDB(collection_name, db_name)
